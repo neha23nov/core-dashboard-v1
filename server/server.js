23f0be098c9266eed3de211ec12ce26d0ccef4fe
Neha Saini <145911161+neha23nov@users.noreply.github.com>
2026-01-21 00:05:57 +0530

    update

--- server/server.js
+++ server/server.js
@@ -21,10 +21,11 @@ const app=express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*", // allow deployed frontend
     credentials: true,
   })
 );
+
 
 app.use("/api/students", studentRoutes);
 app.use("/api/attendance", attendanceRoutes);
@@ -36,12 +37,7 @@ const PORT = process.env.PORT || 5000;
 
 
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB connected");
-    app.listen(5000, () =>
-      console.log("Server running on http://localhost:5000")
-    );
-  })
-  .catch((err) => console.error(err));
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
+
